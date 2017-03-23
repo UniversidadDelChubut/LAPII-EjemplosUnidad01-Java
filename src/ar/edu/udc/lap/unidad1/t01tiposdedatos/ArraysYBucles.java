d2f2d700439e63181d7048ce4b3cfa8c82de590c
Santiago Della Torre <sdellatorre@gmail.com>
2017-03-23 20:04:06 -0300

    Primer commit

---
+++ src/ar/edu/udc/lap/unidad1/t01tiposdedatos/ArraysYBucles.java
@@ -0,0 +1,80 @@
+package ar.edu.udc.lap.unidad1.t01tiposdedatos;
+
+/**
+ * Ejemplos de arrays y estructuras repetitivas.
+ * 
+ * @see https://docs.oracle.com/javase/tutorial/java/nutsandbolts/arrays.html
+ */
+public class ArraysYBucles {
+
+	public static void main(String[] args) {
+
+		float[] tempertaturasPromedio = new float[12];
+
+		tempertaturasPromedio [0] = 23.4f;
+		tempertaturasPromedio [1] = 22.1f;
+		tempertaturasPromedio [2] = 20.0f;
+		tempertaturasPromedio [3] = 17.0f;
+		tempertaturasPromedio [4] = 13.0f;
+		tempertaturasPromedio [5] = 10.4f;
+		tempertaturasPromedio [6] =  9.5f;
+		tempertaturasPromedio [7] = 11.5f;
+		tempertaturasPromedio [8] = 13.7f;
+		tempertaturasPromedio [9] = 17.5f;
+		tempertaturasPromedio[10] = 20.6f;
+		tempertaturasPromedio[11] = 22.9f;
+		
+		
+		/* ¿cuál es el promedio anual? */
+		
+		
+		/*	Se pronostica que de aquí a 30 años las temperaturas aumentraán en un 12 %
+		 *  ¿Cómo quedaría el arreglo de temperaturas medias para entonces? 
+		 */
+		 
+		
+		/* Ejercicio
+		 * Cree un arreglo con las temperaturas promedio expresada en grados farenheith
+		 */
+		
+
+		int[] fibonacci = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 };
+		/* Ejercicio escriba una rutuna que, dado un número entero, escriba la sucesión de fibonacci de ese órden */
+		
+
+		/*
+		while(true)
+			;
+		*/
+		
+		/*
+		int i = 0;
+		
+		while(i< 10){
+			System.out.println(i);
+			i++;
+		}*/
+		
+		/*
+		int i = 0;
+		
+		do {
+			System.out.println(i);
+			i++;
+		} while (i< 10);
+		*/
+		
+		int i = 0;
+		
+		do
+			System.out.println(i++);	
+		while (i< 10);
+				
+		/*
+		for (int i = 0; i < 10 ; i++){
+			System.out.println(i);
+		}*/
+		
+	}
+
+}
