f59243f791d33999c7a432d18007323c74a08901
Santiago Della Torre <sadellatorre@issys.gov.ar>
2018-03-23 13:31:02 -0300

    Se convierte ejemplo de encuesta en ejemplo de preferencias de usuario

--- src/ar/edu/udc/lap/unidad1/bitwise/TestPreferencias.java
+++ src/ar/edu/udc/lap/unidad1/bitwise/TestPreferencias.java
@@ -2,11 +2,13 @@ package ar.edu.udc.lap.unidad1.bitwise;
 
 
 
-public class TestEncuesta {
+public class TestPreferencias {
 
 	public static void main(String[] args) {
 		PreferenciasUsuario prefs = new PreferenciasUsuario("sdellatorre");
 		prefs.setSugerenciasBusqueda(true);
+		prefs.setMostrarMensajesAyuda(true);
+		prefs.setSugerenciasBusqueda(false);
 		System.out.println(prefs);
 	}
 	
