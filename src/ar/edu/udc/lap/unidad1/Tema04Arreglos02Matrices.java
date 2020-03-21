6aee2a812f85c9d67f6f146da0ccce5f132657e6
Santiago Della Torre <sdellatorre@gmail.com>
2020-03-21 06:56:09 -0300

    Se modifican ejemplos de matrices

--- src/ar/edu/udc/lap/unidad1/Tema04Arreglos02Matrices.java
+++ src/ar/edu/udc/lap/unidad1/Tema04Arreglos02Matrices.java
@@ -24,12 +24,32 @@ public class Tema04Arreglos02Matrices {
 		}	
 		System.out.println();
 		
-		for (int[]fila: b) {  //número de filas
-		     for (int v: fila) { //número de columnas de cada fila
+		for (int[]fila: b) { 
+		     for (int v: fila) { 
 		          System.out.print(v + " ");
 		     }
 		     System.out.println();
 		}	
+		System.out.println();
+		
+		int [][] c = new int[3][];
+		c[0] = new int[1];
+		c[1] = new int[2];
+		c[2] = new int[3];
+		c[0][0]=1;
+		c[1][0]=1;
+		c[1][1]=1;
+		c[2][0]=1;
+		c[2][1]=2;
+		c[2][2]=1;
+		
+		for (int[]fila: c) {  
+		     for (int v: fila) { 
+		          System.out.print(v + " ");
+		     }
+		     System.out.println();
+		}	
+		
 		
 	}
 	
